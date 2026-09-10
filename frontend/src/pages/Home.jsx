6af65f62f17b1d2a65935bc10528e944e3d2f563
JoyalKumar-31 <janampallyjoyalkumar99@gmail.com>
2026-09-10 11:14:26 +0530

    Update BIS Assist frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
-
+import Assistant from '../components/Assistant'
 function Home() {
   return (
     <>
       <Navbar />
       <Hero />
       <Features />
+      <Assistant />
     </>
   )
 }
